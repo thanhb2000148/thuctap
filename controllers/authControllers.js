a0b1050605df0d99dd4deb65f86fd13e8b473b3d
B2000144 <trib2000144@student.ctu.edu.vn>
2024-05-15 10:15:01 +0700

    register add user + account

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,18 +1,34 @@
 const account = require("../models/account");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const authController = {
   registerUser: async (req, res) => {
     try {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(req.body.password, salt);
-      // create a new account
-      const newAccount = await new account({
+      //create a new user
+      const newUser = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email_user: req.body.email_user,
+        address_user: req.body.address_user,
+        phone_number: req.body.phone_number,
+        created_at: new Date(),
+        gender_user: req.body.gender_user,
+        middle_name: req.body.middle_name,
+        full_name: req.body.full_name,
+        avt: req.body.avt,
+      });
+      const savedUser = await newUser.save();
+      // create a new account link it to the user
+      const newAccount = new account({
         user_name: req.body.user_name,
         password: hashedPassword,
+        user_id: savedUser._id,
       });
       // save the account
-      const Account = await newAccount.save();
-      res.status(201).json(Account);
+      const savedAccount = await newAccount.save();
+      res.status(201).json({ account: savedAccount, user: savedUser });
     } catch (e) {
       res.status(500).json({
         message: e.message,
