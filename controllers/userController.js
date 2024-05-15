86655b4eff185999577621052da9e9b3780bbc35
B2000144 <trib2000144@student.ctu.edu.vn>
2024-05-15 16:53:52 +0700

    deleteAccount

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,39 @@
 const account = require("../models/account");
-
+const user = require("../models/user");
 const userController = {
   getAllUsers: async (req, res) => {
     try {
       const User = await account.find().populate("user_id");
       res.status(200).json(User);
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
+  },
+  getAUser: async (req, res) => {
+    try {
+      const User = await account.findById(req.params.id).populate("user_id");
+      res.status(200).json(User);
+    } catch (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const accountToDelete = await account.findByIdAndDelete(req.params.id);
+      if (!accountToDelete) {
+        return res.status(404).json({
+          message: "account not found",
+        });
+      }
+      if (accountToDelete.user_id) {
+        await user.findByIdAndDelete(accountToDelete.user_id);
+      }
+      await account.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "User deleted" });
     } catch (err) {
       res.status(500).json({
         message: err.message,
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,6 @@
 const userController = require("../controllers/userController");
 const router = require("express").Router();
 router.get("/", userController.getAllUsers);
-
+router.get("/:id", userController.getAUser);
+router.delete("/:id", userController.deleteUser);
 module.exports = router;
